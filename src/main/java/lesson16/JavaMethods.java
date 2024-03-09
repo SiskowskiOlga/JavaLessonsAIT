4f11432a7202a9efe412dc5902b53141d4a7accb
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-03-09 22:28:32 +0100

    тренируем методы. varargs

--- src/main/java/lesson16/JavaMethods.java
+++ src/main/java/lesson16/JavaMethods.java
@@ -1,29 +1,39 @@
 package lesson16;
 
 public class JavaMethods {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         printNames("Anna");
         printNames("Max", "Olga");
 
+
     }
-    public int sum(int a, int b){
-        return  a+b;
+
+    public int sum(int a, int b) {
+
+        return a + b;
     }
-    public static double sum(double a, double b){
-        return  a+b;
+
+    public static double sum(double a, double b) {
+
+        return a + b;
     }
-    static int yearSalary = 0;
-    public static int addSalary(int monthSalary){
+
+    static int yearSalary = 0; //параметр переменная
+
+    //метод складывает основную зарплату и передает в переменную
+    public static int addSalary(int monthSalary) {
         return yearSalary = yearSalary + monthSalary;
 
     }
-    public static int addSalary(int monthSalary, int smallSalary){
+
+    //если еще дополнительно работает к зарплате
+    public static int addSalary(int monthSalary, int smallSalary) {
         return yearSalary = yearSalary + monthSalary + smallSalary;
     }
 
-    // ... ---> передаем любое количество переменных)
-    static void  printNames (String... names){
-        for (String name : names){
+    // ... --->не знаем точно сколько хотим передать параметров с ... передаём любое количество переменных
+    static void printNames(String... names) {
+        for (String name : names) {
             System.out.println(name);
         }
     }
