fbd092809d68be564f11c8930122194f9bb12318
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-03-10 20:41:55 +0100

    valueOf and parsenInt comparison

---
+++ src/main/java/konsultation/ChangeTypes.java
@@ -0,0 +1,17 @@
+package konsultation;
+
+public class ChangeTypes {
+    public static void main(String[] args) {
+        int number = 3000;
+        String numberString = String.valueOf(number);
+
+        Integer numberTwo = Integer.valueOf(numberString);// объектный большой интежер
+        int numberThree = Integer.parseInt(numberString); //приметивный маленький интежер
+
+        System.out.println(numberString);
+        System.out.println(numberTwo);
+        System.out.println(numberThree);
+
+    }
+}
+
