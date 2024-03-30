e79252e00a737c4bb1a7d1a2cd3adeb519f2609c
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-03-30 22:55:12 +0100

    created a Calculator, using switch case

---
+++ src/main/java/lesson09/Calculator.java
@@ -0,0 +1,28 @@
+package lesson09;
+
+public class Calculator {
+    private static String operation = "+";
+    private static double numberOne = 10;
+    private static double numberTwo = 30;
+
+    public static void main(String[] args) {
+        double result;
+
+        switch (operation) {
+            case "+":
+                result = sumNumbers(numberOne, numberTwo);
+                break;
+            default:
+                System.out.println("Неправильная операция");
+                result = 0;
+        }
+
+        System.out.println(result);
+    }
+
+    public static double sumNumbers(double firstNumber, double secondNumber) {
+        double result = firstNumber + secondNumber;
+        return result;
+
+    }
+}
