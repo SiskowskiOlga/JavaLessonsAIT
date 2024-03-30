7bda83649315e0f76ed4c606521584da69b53691
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-03-30 23:35:07 +0100

    created a test of clean code

---
+++ src/main/java/lesson05/CleanCode.java
@@ -0,0 +1,13 @@
+package lesson05;
+
+public class CleanCode {
+    public static void main(String[] args) {
+        int numberX = 20;
+        int numberY = 30;
+        int numberZ = 40;
+
+        if (numberX < numberY && numberY < numberZ);
+
+        System.out.println (numberX + "<" + numberY + "<" + numberZ);
+    }
+}
