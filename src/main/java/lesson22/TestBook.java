e99e0c8a68829dc0fe630713026c8ee7f2f234ce
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-03-30 23:26:45 +0100

    create a HashSet

---
+++ src/main/java/lesson22/TestBook.java
@@ -0,0 +1,13 @@
+package lesson22;
+import  java.util.HashSet;
+public class TestBook {
+    public static void main(String[] args) {
+        Book pilevinBook = new Book("KGBT+","Pivevin");
+        Book pilevinNewBook = new Book("KGBT+","Pilevin");
+        Book bookHobbit = new lesson22.Book("Hobbit","Tolkien");
+
+        HashSet<Book> bookHashSet = new HashSet<>();
+        
+    }
+
+}
