cace7c47506af795599ded92a9c19fa127ae31da
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-03-30 23:33:24 +0100

    created a rectangle to calculate parameters

---
+++ src/main/java/lesson04/PerimeterOfRectangle.java
@@ -0,0 +1,24 @@
+package lesson04;
+
+public class PerimeterOfRectangle {
+
+
+    /* Объявите две переменные для длин сторон прямоугольника.
+    Написать код для вычисления периметра прямоугольника и выведите результат.
+     */
+    public static void main(String[] args) {
+
+        int length = 4;
+        int weight = 5;
+        int out = (length * 2) + (weight * 2);
+        System.out.println(out);
+
+
+    }
+
+}
+
+
+
+
+
