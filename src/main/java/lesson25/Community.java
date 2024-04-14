0a3f9a4635a2d40da0c19161c3d5ec32aa46f690
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-04-14 02:23:23 +0200

    социальная сеть с использованием HashSet, HashMap, Iterator

---
+++ src/main/java/lesson25/Community.java
@@ -0,0 +1,36 @@
+package lesson25;
+import java.util.HashSet;
+import java.util.HashMap;
+public class Community {
+    private String name;
+
+    private HashSet<User> members;
+
+    public static HashMap<String, Community> communityHashMap = new HashMap<>();
+
+    public Community(String name) {
+        this.name = name;
+        this.members = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HashSet<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(HashSet<User> members) {
+        this.members = members;
+    }
+
+    public void addMember(User user) {
+        members.add(user);
+    }
+
+}
