bb757726e75a3896cde11fc37f9792af451940b9
SiskowskiOlga <160174931+SiskowskiOlga@users.noreply.github.com>
2024-06-03 20:51:22 +0200

    created Logger, Scanner,

---
+++ src/main/java/homework32/Student.java
@@ -0,0 +1,20 @@
+package homework32;
+
+public class Student {
+    String name;
+    int grade;
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+}
